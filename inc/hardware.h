e8d936770658ac77bfcf18ca8b74a23b35360c47
RedropBolivia <benjaminsoruco685@gmail.com>
2025-12-09 03:46:00 -0400

    ultimo arregalo en hardware.h

--- inc/hardware.h
+++ inc/hardware.h
@@ -5,22 +5,22 @@
 #include <Wire.h>
 #include "buttons.h"
 
-// ============ PINES BTS7960 (Motor) ============
+// PINES BTS7960 (Motor)
 extern const uint8_t PIN_MOTOR_REN;
 extern const uint8_t PIN_MOTOR_LEN;
 extern const uint8_t PIN_MOTOR_RPWM;
 extern const uint8_t PIN_MOTOR_LPWM;
 
-// ============ 2 SENSORES ============
+//  2 SENSORES 
 extern const uint8_t PIN_HOME;        // Sensor HOME (posición 1)
 extern const uint8_t PIN_COUNTER;     // Único sensor contador
 
-// ============ SERVO ============
+// SERVO 
 extern const uint8_t PIN_SERVO;
 extern const int SERVO_REST_ANGLE;
 extern const int SERVO_DISPENSE_ANGLE;
 
-// ============ BOTONES ============
+// BOTONES
 extern const uint8_t PIN_BTN_UP;
 extern const uint8_t PIN_BTN_DOWN;
 extern const uint8_t PIN_BTN_OK;
